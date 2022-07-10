043990799f41753cd81c8f2e7c5c8be0b0b4522b
suphaloet <suphaloet.skp@gmail.com>
2022-07-11 00:54:35 +0700

    faculty member page

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 import styles from "/styles/Layout.module.css";
 
 function Navbar() {
-
   const [active, setActive] = useState(false);
-
+  const [curriculumActive, setCurriculumActive] = useState(false);
+  const [memberActive, setMemberActive] = useState(false);
   const handleMobileMenuClick = () => {
-    setActive(!active)
+    setActive(!active);
+  };
+
+  const handlecurriculumClick = () => {
+    setCurriculumActive(!curriculumActive)
+  }
+
+  const handleMemberClick = () => {
+    setMemberActive(!memberActive)
   }
 
   return (
@@ -20,115 +28,151 @@ function Navbar() {
           </div>
 
           <div className="hidden md:flex items-center space-x-1">
-            <a href="/" className={styles.menuItem}>Home</a>
-            <a href="/about" className={styles.menuItem}>About</a>
-            <a href="/how_to" className={styles.menuItem}>Curriculum</a>
-            <a href="/about_us" className={styles.menuItem}>Members</a>
-            <a href="/comming_soon" className={styles.menuItem}>Research</a>
-            <a href="/contact" className={styles.menuItem}>Resources</a>
-            <a href="/contact" className={styles.menuItem}>Contact</a>
-            <a href="/" className={styles.menuItem}>EN</a>
-            <a href="/" className={styles.menuItem}>Search</a>
+            <a href="/" className={styles.menuItem}>
+              Home
+            </a>
+            <a href="/about" className={styles.menuItem}>
+              About
+            </a>
+            <a
+              href="#"
+              onClick={handlecurriculumClick}
+              className={styles.menuItem}
+            >
+              Curriculum
+            </a>
+            <a href="#" onClick={handleMemberClick} className={styles.menuItem}>
+              Members
+            </a>
+            <a href="/comming_soon" className={styles.menuItem}>
+              Research
+            </a>
+            <a href="/contact" className={styles.menuItem}>
+              Resources
+            </a>
+            <a href="/contact" className={styles.menuItem}>
+              Contact
+            </a>
+            <a href="/" className={styles.menuItem}>
+              EN
+            </a>
+            <a href="/" className={styles.menuItem}>
+              Search
+            </a>
           </div>
 
+          {/* Curriculum  */}
+          <div className={`${curriculumActive?"":"hidden"} mobile-menu right-0 top-24 bg-white absolute border-pink-900 border-b-2 border-l-2 w-1/2 z-50`}>
+            <ul className="">
+              <li className="active">
+                <a href="/" className={`block text-sm px-2 py-4`}>
+                  Bachelor
+                </a>
+              </li>
+              <li className="">
+                <a href="/" className={`block text-sm px-2 py-4`}>
+                  Master
+                </a>
+              </li>
+              <li className="">
+                <a href="/" className={`block text-sm px-2 py-4`}>
+                  Doctoral
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* member  */}
+          <div className={`${memberActive?"":"hidden"} mobile-menu right-0 top-24 bg-white absolute border-pink-900 border-b-2 border-l-2 w-1/2 z-50`}>
+            <ul className="">
+              <li className="">
+                <a href="/member/faculty" className={`block text-sm px-2 py-4`}>
+                  Faculty Members
+                </a>
+              </li>
+              <li className="">
+                <a href="/" className={`block text-sm px-2 py-4`}>
+                  Supporting Staff
+                </a>
+              </li>
+            </ul>
+          </div>
           {/* <!-- Mobile menu button --> */}
           <div className="md:hidden flex items-center">
-          <button
-                className="outline-none mobile-menu-button ml-3 mr-4"
-                onClick={handleMobileMenuClick}
+            <button
+              className="outline-none mobile-menu-button ml-3 mr-4"
+              onClick={handleMobileMenuClick}
+            >
+              <svg
+                className=" w-10 h-10 hover:text-green-500 "
+                x-show="!showMenu"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  className=" w-10 h-10 hover:text-green-500 "
-                  x-show="!showMenu"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-              </button>
-            </div>
+                <path d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
 
-       {/* <!-- mobile menu --> */}
+      {/* <!-- mobile menu --> */}
 
-       <div
-          className={`${active?"":"hidden"} mobile-menu right-0 bg-white absolute border-pink-900 border-b-2 border-l-2 w-1/2 z-50`}
-        >
-          <ul className="">
-            <li className="active">
-              <a
-                href="/"
-                className={`block text-sm px-2 py-4`}
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/about"
-                className={`block text-sm px-2 py-4 `}
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="/how_to"
-                className={`block text-sm px-2 py-4 `}
-              >
-                Curriculum
-              </a>
-            </li>
-            <li>
-              <a
-                href="/about_us"
-                className={`block text-sm px-2 py-4 `}
-              >
-               Members
-              </a>
-            </li>
-            <li>
-              <a
-                href="/comming_soon"
-                className={`block text-sm px-2 py-4  `}
-              >
-                Research
-              </a>
-            </li>
-            <li>
-              <a
-                href="/contact"
-                className={`block text-sm px-2 py-4  `}
-              >
-                Resources
-              </a>
-            </li>
-            <li>
-              <a
-                href="/contact"
-                className={`block text-sm px-2 py-4  `}
-              >
-                Contact
-              </a>
-            </li>
-            <li>
-              <hr className="bg-black"></hr>
-            </li>
-            <li>
-              <a
-                href="/contact"
-                className={`block text-sm px-2 py-4  `}
-              >
-                EN
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div
+        className={`${
+          active ? "" : "hidden"
+        } mobile-menu right-0 bg-white absolute border-pink-900 border-b-2 border-l-2 w-full z-50`}
+      >
+        <ul className="">
+          <li className="active">
+            <a href="/" className={`block text-sm px-2 py-4`}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/about" className={`block text-sm px-2 py-4 `}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={handlecurriculumClick} className={`block text-sm px-2 py-4 `}>
+              Curriculum
+            </a>
+          </li>
+          <li>
+            <a href="#"  onClick={handleMemberClick} className={`block text-sm px-2 py-4 `}>
+              Members
+            </a>
+          </li>
+          <li>
+            <a href="/comming_soon" className={`block text-sm px-2 py-4  `}>
+              Research
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className={`block text-sm px-2 py-4  `}>
+              Resources
+            </a>
+          </li>
+          <li>
+            <a href="/contact" className={`block text-sm px-2 py-4  `}>
+              Contact
+            </a>
+          </li>
+          <li>
+            <hr className="bg-black"></hr>
+          </li>
+          <li>
+            <a href="/contact" className={`block text-sm px-2 py-4  `}>
+              EN
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
---
+++ components/member/FacultyMemberCard.js
@@ -0,0 +1,16 @@
+function FacultyMemberCard() {
+    return (
+        <div className="bg-white p-4">
+            <img src="/demo/member/02-CK.jpg" className="w-full" />
+            <div className="mt-4">
+                <h3>Associate Professor</h3>
+                <h3>Chonnanath Kritworakarn, Ph.D.</h3>
+                <h3>Industrial Engineering</h3>
+                <p>chonnanath.krit@gmail.com</p>
+                <p>053-944125, 053-944183,053-944126 ต่อ 304</p>
+            </div>
+        </div>
+    )
+}
+
+export default FacultyMemberCard
