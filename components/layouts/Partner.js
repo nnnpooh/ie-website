ddb21437df2a3c4a46cbf9f2fdd9238d744d472b
suphaloet <suphaloet.skp@gmail.com>
2022-07-10 23:50:33 +0700

    fix partner for mobile

--- components/layouts/Partner.js
+++ components/layouts/Partner.js
@@ -1,29 +1,29 @@
 function Partner() {
   return (
-    <div className="section_container mx-auto px-52">
+    <div className="section_container mx-auto md:px-52">
       <div className="text-center">
         <h1 className="section_title text-white">Sponsors and partners</h1>
       </div>
       <div className="flex flex-wrap mt-12">
-        <div className="w-1/4 p-2">
+        <div className="md:w-1/4 w-1/2 p-2">
           <img
             src="/demo/partner/msie 280110.png"
             className="w-full rounded-lg"
           />
         </div>
-        <div className="w-1/4 p-2">
+        <div className="md:w-1/4 w-1/2 p-2">
           <img
             src="/demo/partner/bannerddru_280x1101.jpg"
             className="w-full rounded-lg"
           />
         </div>
-        <div className="w-1/4 p-2">
+        <div className="md:w-1/4 w-1/2 p-2">
           <img
             src="/demo/partner/bannerstep_280x1101.jpg"
             className="w-full rounded-lg"
           />
         </div>
-        <div className="w-1/4 p-2">
+        <div className="md:w-1/4 w-1/2 p-2">
           <img
             src="/demo/partner/logis.jpg"
             className="w-full rounded-lg"
