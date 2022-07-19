0b9f453070b18934390045db2ef2ec7f82c5a711
Nirand <nnnpooh@gmail.com>
2022-07-19 12:59:45 +0700

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,47 +11,55 @@ import {
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      theme={{ fontFamily: 'Prompt' }}
-      withGlobalStyles
-      withNormalizeCSS
-    >
-      <AppShell
-        padding='md'
-        navbar={
-          <Navbar width={{ base: 300 }} height={500} p='xs'>
-            <ul>
-              <li>
-                <Link href='/'>
-                  <a>Home</a>
-                </Link>
-              </li>
-
-              <li>
-                <Link href='/faculty'>
-                  <a>Faculty</a>
-                </Link>
-              </li>
-            </ul>
-          </Navbar>
-        }
-        header={
-          <Header height={60} p='xs'>
-            <Title>IE Website Test</Title>
-          </Header>
-        }
-        styles={(theme) => ({
-          main: {
-            backgroundColor:
-              theme.colorScheme === 'dark'
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-          },
-        })}
+    <>
+      <MantineProvider
+        theme={{ fontFamily: 'Prompt' }}
+        withGlobalStyles
+        withNormalizeCSS
       >
-        <Component {...pageProps} />
-      </AppShell>
-    </MantineProvider>
+        <AppShell
+          padding='md'
+          navbar={
+            <Navbar width={{ base: 300 }} height={500} p='xs'>
+              <ul>
+                <li>
+                  <Link href='/'>
+                    <a>Home</a>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link href='/faculty'>
+                    <a>Faculty</a>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link href='/research_center'>
+                    <a>Research Center</a>
+                  </Link>
+                </li>
+              </ul>
+            </Navbar>
+          }
+          header={
+            <Header height={60} p='xs'>
+              <Title>IE Website Test</Title>
+            </Header>
+          }
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}
+        >
+          <Component {...pageProps} />
+        </AppShell>
+      </MantineProvider>
+    </>
   );
 }
 
---
+++ pages/research_center.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import { gql } from '@apollo/client';
+import client from '../apollo-client';
+import { NextPage } from 'next';
+import { RootQuery, Maybe } from '../src/generated/graphql';
+import { Card, Title, Text, Badge, Table } from '@mantine/core';
+import Image from 'next/image';
+import profileDefault from '../public/profile_default.jpg';
+
+interface props {
+  data: Pick<RootQuery, 'researchCenters'>;
+}
+
+const ResearchCenter: NextPage<props> = ({ data }) => {
+  const temp = data.researchCenters?.nodes?.map((el) => ({
+    id: el?.databaseId,
+    ...el,
+  }));
+
+  const reses = temp?.map((res) => ({
+    ...res,
+  }));
+
+  console.log({ data: data.researchCenters, reses });
+  return (
+    <>
+      <div
+        className='prose prose-img:rounded-xl prose-headings:underline prose-a:text-blue-600'
+        dangerouslySetInnerHTML={{ __html: reses[0].content }}
+      />
+    </>
+  );
+};
+
+export default ResearchCenter;
+
+export async function getStaticProps() {
+  const { data } = await client.query<RootQuery>({
+    query: gql`
+      query ResearchCenterQuery {
+        researchCenters {
+          nodes {
+            content
+            databaseId
+            title
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
