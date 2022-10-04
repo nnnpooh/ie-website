a561580723e9cfc847346b21ebf65ec74d599b1d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-10-04 21:17:46 +0700

    working on faculty card

--- src/components/member/FacultyMemberCard.tsx
+++ src/components/member/FacultyMemberCard.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import Image from "next/image";
-import { Card, Center } from "@mantine/core";
+import { Card, Center, Badge } from "@mantine/core";
 import { useRouter } from "next/router";
+import { IconMail, IconPhone } from "@tabler/icons";
 import { FacultyType } from "@api/types/manual";
 
 interface Props {
@@ -13,7 +14,16 @@ const FacultyMemberCard: FC<Props> = ({ fac }) => {
   const lang = router.locale;
 
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
+    <Card
+      shadow="sm"
+      p="lg"
+      radius="md"
+      withBorder
+      className="cursor-pointer hover:scale-101 transition-transform"
+      onClick={() => {
+        router.push(`/people/faculty/${fac.databaseId}`);
+      }}
+    >
       <Card.Section>
         <Center
           className="relative h-24 w-full"
@@ -40,10 +50,34 @@ const FacultyMemberCard: FC<Props> = ({ fac }) => {
         </Center>
       </Card.Section>
       <div className="mt-24">
-        <div className="text-md font-bold text-primary">
-          {lang === "th" ? fac.fullNameTh : fac.fullNameEn}
+        <div className="flex flex-col items-center">
+          <div className="text-md font-bold text-primary">
+            {lang === "th" ? fac.fullNameTh : fac.fullNameEn}
+          </div>
+          <div className="flex gap-2">
+            {fac.adminPositionTh.map((el) => (
+              <Badge>{el}</Badge>
+            ))}
+          </div>
         </div>
-        <h3>{fac.adminPositionTh}</h3>
+
+        <div className="grid grid-cols-12 items-center">
+          <IconMail size={15} className="col-span-1 text-primary" />
+          <div className="col-span-11 flex gap-x-2 flex-wrap text-sm">
+            {fac.email.map((el) => (
+              <div key={el}>{el}</div>
+            ))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-12 items-center">
+          <IconPhone size={15} className="col-span-1 text-primary" />
+          <div className="col-span-11 flex gap-x-2 flex-wrap text-sm">
+            {fac.phone.map((el) => (
+              <div key={el}>{el}</div>
+            ))}
+          </div>
+        </div>
       </div>
     </Card>
   );
