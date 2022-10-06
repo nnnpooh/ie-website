5797ace0a72905cd8938db43ebf24395aad0d777
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-10-06 07:15:24 +0700

    Implement horizontal scrolling

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -1,7 +1,8 @@
-import { NextPage } from "next";
-import { FBFeedType } from "@src/api";
-import Image from "next/image";
-import { Container, Card } from "@mantine/core";
+import { NextPage } from 'next';
+import { FBFeedType } from '@src/api';
+import Image from 'next/image';
+import ScrollContainer from 'react-indiana-drag-scroll';
+import { Container, Card, ScrollArea } from '@mantine/core';
 
 interface Props {
   ug: FBFeedType;
@@ -11,19 +12,35 @@ interface Props {
 
 const NewsPage: NextPage<Props> = ({ ug, im, grad }) => {
   console.log({ ug, im, grad });
+
+  const ts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   return (
-    <div className="bg-gray-200">
-      <Container size={"xl"}>
-        <div className="flex gap-2">
+    <div className='bg-gray-200'>
+      <ScrollContainer>
+        <div className='flex gap-2'>
+          {ts.map((el) => (
+            <div
+              className='flex h-80 w-80 bg-red-300 items-center justify-center flex-none'
+              style={{ width: 500 }}
+              key={el}
+            >
+              {el}
+            </div>
+          ))}
+        </div>
+      </ScrollContainer>
+
+      {/* <Container size={'xl'}>
+        <div className='flex gap-2'>
           {ug.data.map((fb) => (
             <Card key={fb.id}>
               <Card.Section>
                 {fb?.full_picture && (
-                  <div className="relative h-80 w-80">
+                  <div className='relative h-80 w-80'>
                     <Image
                       src={fb.full_picture}
-                      layout="fill"
-                      objectFit="cover"
+                      layout='fill'
+                      objectFit='cover'
                     />
                   </div>
                 )}
@@ -31,12 +48,12 @@ const NewsPage: NextPage<Props> = ({ ug, im, grad }) => {
               <div>
                 {fb?.attachments?.data
                   ? fb.attachments.data[0].description
-                  : ""}
+                  : ''}
               </div>
             </Card>
           ))}
         </div>
-      </Container>
+      </Container> */}
     </div>
   );
 };
