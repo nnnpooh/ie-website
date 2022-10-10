29bf4e7aa28a998a371f6cea0fa4a4e2fbb00d53
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-10-11 03:55:55 +0700

    add new domain

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://10.10.12.140:81/?graphql=true",
+  uri: "https://ieadmin.eng.cmu.ac.th/graphql",
   cache: new InMemoryCache(),
 });
 
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextTranslate = require("next-translate");
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["10.10.12.140", "localhost"],
+    domains: ["10.10.12.140", "localhost", "ieadmin.eng.cmu.ac.th"],
   },
   i18n: {
     locales: ["en", "th"],
